0c08cc1519501ac1f1118f349fda560bb0245c51
Yasintha Perera <yasintha94@gmail.com>
2017-06-09 08:21:28 +0530

    final

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-//post body ---> conver to json
+//post body --> convert to json
 var bodyParser = require('body-parser');
 
 //Mongodb ORM
@@ -8,28 +8,89 @@ var mongoose = require('mongoose');
 var app = express();
 
 var Task = require('./models/task.model');
+
 mongoose.connect('mongodb://127.0.0.1:27017/todoApp');
 
-app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/hello', function(req,res){
-    res.send('Hello Node!');
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.get('/hello', function (req, res){
+    res.send('Hello World');
 });
 
-app.post('/task/add', function(req, res){
+// add
+app.post('/task/add', function (req, res){
+    var title = req.body.title;
+    var done = req.body.done;
+
+    console.log(req.body);
     var task = new Task({
-        title: req.body.title,
-        done: req.body.done
+        title: title,
+        done: done
     });
 
-    task.save(function(err,data){
+    task.save(function (err, data){
         if(err){
-            res.status(400).json({error: err});
+            res.status(400).json({ error: err });
         }
-        res.json({status: 'success', data:data});
+        res.json({ status: 'success', data: data});
     });
 });
 
-app.listen(3100,()=>{
-    console.log("Sever up and running at "+3100);
+//get
+app.get('/task/list',function(req,res){
+    Task.find({},function(err,data){
+            if(err){
+            res.status(400).json({ error: err });
+        }
+        res.json({ status: 'success', data: data});
+    });
+})
+
+// view
+
+app.get("/task/view/:id",function(req,res){
+    Task.findById(req.params.id,function(err,data){
+
+                    if(err){
+            res.status(400).json({ error: err });
+        }
+        res.json({ status: 'success', data: data});
+    })
 });
+
+// update
+app.put('/task/update/:id',function(req,res){
+    Task.findById(req.params.id, function(err,task){
+        if(err){
+               res.status(400).json({ error: err });
+        }
+
+        task.title = req.body.title;
+        task.done= req.body.done;
+
+        task.save(function(err,data){
+ if(err){
+            res.status(400).json({ error: err });
+        }
+        res.json({ status: 'success', data: data});
+        });
+    })
+})
+
+//delete
+app.delete('/task/delete/:id',function(req,res){
+     Task.findByIdAndRemove(req.params.id, function(err,task){
+ 
+ if(err){
+            res.status(400).json({ error: err });
+        }
+        res.json({ status: 'success', data: task});
+        })
+    
+})
+
+app.listen(3200, () => {
+    console.log("server started at" + 3200);
+} );
